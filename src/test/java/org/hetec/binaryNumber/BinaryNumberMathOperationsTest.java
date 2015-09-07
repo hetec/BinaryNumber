df95110f8d100b65a771ab14bf4c5ca3e1fcfb2e
hetec <hebner.tec@gmail.com>
2015-09-07 12:24:50 +0200

    Refactor test for math methods

--- src/test/java/org/hetec/binaryNumber/BinaryNumberMathOperationsTest.java
+++ src/test/java/org/hetec/binaryNumber/BinaryNumberMathOperationsTest.java
@@ -1,230 +1,210 @@
 package org.hetec.binaryNumber;
 import static org.junit.Assert.assertEquals;
 
-import java.math.BigInteger;
 
 import org.hetc.binaryNumber.BinaryNumber;
 import org.junit.Before;
 import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.*;
+import org.junit.BeforeClass;
 
 public class BinaryNumberMathOperationsTest {
 
-	private BinaryNumber six;
-	private BinaryNumber minusSix;
-	private BinaryNumber seven;
-	private BinaryNumber zero;
-	private BinaryNumber minusSeven;
+	private static BinaryNumber six;
+	private static BinaryNumber minusSix;
+	private static BinaryNumber seven;
+	private static BinaryNumber zero;
+	private static BinaryNumber minusSeven;
+        private static BinaryNumber thirty;
+        private static BinaryNumber minusThirty;
 
-	@Before
-	public void setUp(){
-		six = BinaryNumber.of(6);
-		seven = BinaryNumber.of(7);
-		zero = BinaryNumber.of(0);
-		minusSix = BinaryNumber.of(-6);
-		minusSeven = BinaryNumber.of(-7);
+	@BeforeClass
+	public static void setUp(){
+            six = BinaryNumber.of(6);
+            seven = BinaryNumber.of(7);
+            zero = BinaryNumber.of(0);
+            minusSix = BinaryNumber.of(-6);
+            minusSeven = BinaryNumber.of(-7);
+            thirty = BinaryNumber.of(30);
+            minusThirty = BinaryNumber.of(-30);
 	}
 
 	//Addition
 
 	@Test
 	public void testAdditionWithTwoPosNumbers(){
-		BinaryNumber thriteen = six.add(seven);
-		BinaryNumber thriteen_sym = seven.add(six);
-		assertThat(thriteen.asLong(), is(13L));
-		assertThat(thriteen_sym.asLong(), is(13L));
+            BinaryNumber thriteen = six.add(seven);
+            BinaryNumber thriteen_sym = seven.add(six);
+            assertThat(thriteen.asLong(), equalTo(13L));
+            assertThat(thriteen_sym.asLong(), equalTo(13L));
 	}
 	
 
 	@Test
 	public void testAdditionWithOneNumberAndZero(){
-		BinaryNumber sixPlusZero = six.add(zero);
-		BinaryNumber sixPlusZero_sym = zero.add(six);
-		BinaryNumber minusSixPlusZero = minusSix.add(zero);
-		BinaryNumber zeroPlusMinusSix = zero.add(minusSix);
-		assertThat(sixPlusZero.asLong(), is(6L));
-		assertThat(sixPlusZero_sym.asLong(), is(6L));
-		assertThat(minusSixPlusZero.asLong(), is(-6L));
-		assertThat(zeroPlusMinusSix.asLong(), is(-6L));
+            BinaryNumber sixPlusZero = six.add(zero);
+            BinaryNumber sixPlusZero_sym = zero.add(six);
+            BinaryNumber minusSixPlusZero = minusSix.add(zero);
+            BinaryNumber zeroPlusMinusSix = zero.add(minusSix);
+            assertThat(sixPlusZero.asLong(), equalTo(6L));
+            assertThat(sixPlusZero_sym.asLong(), equalTo(6L));
+            assertThat(minusSixPlusZero.asLong(), equalTo(-6L));
+            assertThat(zeroPlusMinusSix.asLong(), equalTo(-6L));
 	}
 
 	@Test
 	public void testAdditionWithNegAndPosNumber(){
-		BinaryNumber one = minusSix.add(seven);
-		BinaryNumber minusOne = six.add(minusSeven);
-		assertThat(one.asLong(), is(1L));
-		assertThat(minusOne.asLong(), is(-1L));
+            BinaryNumber one = minusSix.add(seven);
+            BinaryNumber minusOne = six.add(minusSeven);
+            assertThat(one.asLong(), equalTo(1L));
+            assertThat(minusOne.asLong(), equalTo(-1L));
 	}
 
 	@Test
 	public void testAdditionWithTwoNegNumbers(){
-		BinaryNumber minusThirteen = minusSix.add(minusSeven);
-		assertThat(minusThirteen.asLong(), is(-13L));
+            BinaryNumber minusThirteen = minusSix.add(minusSeven);
+            assertThat(minusThirteen.asLong(), equalTo(-13L));
 	}
 	
 	//Subtraction
 
 	@Test
 	public void testSubtractWithNumberAndZero(){
-		BinaryNumber result = six.subtract(zero);
-		BinaryNumber result2 = zero.subtract(six);
-		assertEquals("6", result.asBigInt().toString());
-		assertEquals("-6", result2.asBigInt().toString());
+            BinaryNumber sixMinusZero = six.subtract(zero);
+            BinaryNumber zeroMinusSix = zero.subtract(six);
+            assertThat(sixMinusZero.asLong(), equalTo(6L));
+            assertThat(zeroMinusSix.asLong(), equalTo(-6L));
 	}
 
 	@Test
 	public void testSubtractWithTwoPosNumbers(){
-		BinaryNumber result = six.subtract(seven);
-		BinaryNumber result2 = seven.subtract(six);
-		assertEquals("-1", result.asBigInt().toString());
-		assertEquals("1", result2.asBigInt().toString());
+            BinaryNumber minusOne = six.subtract(seven);
+            BinaryNumber one = seven.subtract(six);
+            assertThat(minusOne.asLong(), equalTo(-1L));
+            assertThat(one.asLong(), equalTo(1L));
 	}
 
 	@Test
 	public void testSubtractWithOneNegAndPosNumber(){
-		BinaryNumber result = six.subtract(minusSeven);
-		BinaryNumber result2 = minusSeven.subtract(six);
-		assertEquals("13", result.asBigInt().toString());
-		assertEquals("-13", result2.asBigInt().toString());
+            BinaryNumber thirteen = six.subtract(minusSeven);
+            BinaryNumber minusThirteen = minusSeven.subtract(six);
+            assertThat(thirteen.asLong(), equalTo(13L));
+            assertThat(minusThirteen.asLong(), equalTo(-13L));
 	}
 
 	@Test
 	public void testSubtractWithTwoNegNumbers(){
-		BinaryNumber result = minusSix.subtract(minusSeven);
-		BinaryNumber result2 = minusSeven.subtract(minusSix);
-		assertEquals("1", result.asBigInt().toString());
-		assertEquals("-1", result2.asBigInt().toString());
+            BinaryNumber one = minusSix.subtract(minusSeven);
+            BinaryNumber minusOne = minusSeven.subtract(minusSix);
+            assertThat(one.asLong(), equalTo(1L));
+            assertThat(minusOne.asLong(), equalTo(-1L));
 	}
 
 	//Multiplication
 
 	@Test
 	public void testMultiplyWithTwoPosNumbers(){
-		BinaryNumber result = six.multiply(seven);
-		BinaryNumber result2 = seven.multiply(six);
-		assertEquals("42", result.asBigInt().toString());
-		assertEquals("42", result2.asBigInt().toString());
+            BinaryNumber fourtyTwo = six.multiply(seven);
+            BinaryNumber fourtyTwo_inverted = seven.multiply(six);
+            assertThat(fourtyTwo.asLong(), equalTo(42L));
+            assertThat(fourtyTwo_inverted.asLong(), equalTo(42L));
 	}
 
 	@Test
 	public void testMultiplyWithOnePosAndNegNumber(){
-		BinaryNumber result = six.multiply(minusSeven);
-		BinaryNumber result2 = minusSeven.multiply(six);
-		assertEquals("-42", result.asBigInt().toString());
-		assertEquals("-42", result2.asBigInt().toString());
+            BinaryNumber minusFourtyTwo = six.multiply(minusSeven);
+            BinaryNumber minusFourtyTwo_inverted = minusSeven.multiply(six);
+            assertThat(minusFourtyTwo.asLong(), equalTo(-42L));
+            assertThat(minusFourtyTwo_inverted.asLong(), equalTo(-42L));
 	}
 
 	@Test
 	public void testMultiplyWithTwoNegNumbers(){
-		BinaryNumber result = minusSix.multiply(minusSeven);
-		BinaryNumber result2 = minusSeven.multiply(minusSix);
-		assertEquals("42", result.asBigInt().toString());
-		assertEquals("42", result2.asBigInt().toString());
+            BinaryNumber fourtyTwo = minusSix.multiply(minusSeven);
+            BinaryNumber fourtyTwo_inverted = minusSeven.multiply(minusSix);
+            assertThat(fourtyTwo.asLong(), equalTo(42L));
+            assertThat(fourtyTwo_inverted.asLong(), equalTo(42L));
 	}
 
 	@Test
 	public void testMultiplyWithNumberAndZero(){
-		BinaryNumber result = zero.multiply(seven);
-		BinaryNumber result2 = minusSix.multiply(zero);
-		assertEquals("0", result.asBigInt().toString());
-		assertEquals("0", result2.asBigInt().toString());
+            BinaryNumber local_zero = zero.multiply(seven);
+            BinaryNumber local_zero_inverted = minusSix.multiply(zero);
+            assertThat(local_zero.asLong(), equalTo(0L));
+            assertThat(local_zero_inverted.asLong(), equalTo(0L));
 	}
 
 	@Test
 	public void testMultiplyOfZeros(){
-		BinaryNumber result = zero.multiply(zero);
-		assertEquals("0", result.asBigInt().toString());
+            BinaryNumber local_zero = zero.multiply(zero);
+            assertThat(local_zero.asLong(), equalTo(0L));
 	}
 
 	//invert
 	@Test
 	public void testInvert(){
-		BinaryNumber num = BinaryNumber.of(new byte[]{0,0,1,1,0,1});
-		BinaryNumber inverted = num.invert();
-		assertEquals(BinaryNumber.of(new byte[]{1,1,1,1,0,0,1,0}), inverted);
-		assertEquals(inverted.invert(), num);
+            BinaryNumber num = BinaryNumber.of(new byte[]{0,0,1,1,0,1});
+            BinaryNumber inverted = num.invert();
+            assertThat(BinaryNumber.of(new byte[]{1,1,1,1,0,0,1,0}), equalTo(inverted));
+            assertThat(inverted.invert(), equalTo(num));
 	}
 
 	@Test
 	public void testInvertZero(){
-		BinaryNumber num = BinaryNumber.of(new byte[]{0});
-		BinaryNumber inverted = num.invert();
-		assertEquals(BinaryNumber.of(new byte[]{1}), inverted);
-
+            BinaryNumber num = BinaryNumber.of(new byte[]{0});
+            BinaryNumber inverted = num.invert();
+            assertThat(BinaryNumber.of(new byte[]{1}), equalTo(inverted));
 	}
 
 	//removeLeadingZeros
 
 	@Test
 	public void testRemoveLeadingZeros(){
-		BinaryNumber num = BinaryNumber.of(new byte[]{0,0,1,1,0,1});
-		BinaryNumber num1 = BinaryNumber.of(new byte[]{1,1,0,1});
-		BinaryNumber num2 = BinaryNumber.of(new byte[]{0,0,0,0});
-		assertEquals(BinaryNumber.of(new byte[]{1,1,0,1}), num.removeLeadingZeros());
-		assertEquals(BinaryNumber.of(new byte[]{1,1,0,1}), num1.removeLeadingZeros());
-		assertEquals(BinaryNumber.of(new byte[]{0}), num2.removeLeadingZeros());
+            BinaryNumber leadingZeros = BinaryNumber.of(new byte[]{0,0,1,1,0,1});
+            BinaryNumber noLeadingZeros = BinaryNumber.of(new byte[]{1,1,0,1});
+            BinaryNumber onlyZeros = BinaryNumber.of(new byte[]{0,0,0,0});
+            assertThat(BinaryNumber.of(new byte[]{1,1,0,1}), equalTo(leadingZeros.removeLeadingZeros()));
+            assertThat(BinaryNumber.of(new byte[]{1,1,0,1}), equalTo(noLeadingZeros.removeLeadingZeros()));
+            assertThat(BinaryNumber.of(new byte[]{0}), equalTo(onlyZeros.removeLeadingZeros()));
 	}
 
 	//twosComplement
 
 	@Test
 	public void testTwosComplement(){
-		BinaryNumber num = BinaryNumber.of(new byte[]{0,0,1,1,0,1});
-		BinaryNumber num1 = BinaryNumber.of(new byte[]{0});
-		BinaryNumber num2 = BinaryNumber.of(new byte[]{1,1,0,1});
-		BinaryNumber twos0 = num.towsComplement();
-		BinaryNumber twos1 = num1.towsComplement();
-		BinaryNumber twos2 = num2.towsComplement();
-		assertEquals(BinaryNumber.of(new byte[]{1,1,1,1,0,0,1,1}), twos0);
-		assertEquals(BinaryNumber.of(new byte[]{0}), twos1);
-		assertEquals(BinaryNumber.of(new byte[]{1,1,1,1,0,0,1,1}), twos2);
-
+            BinaryNumber leadingZeros = BinaryNumber.of(new byte[]{0,0,1,1,0,1});
+            BinaryNumber local_zero = BinaryNumber.of(new byte[]{0});
+            BinaryNumber noZeros = BinaryNumber.of(new byte[]{1,1,0,1});
+            assertThat(BinaryNumber.of(new byte[]{1,1,1,1,0,0,1,1}), equalTo(leadingZeros.towsComplement()));
+            assertThat(BinaryNumber.of(new byte[]{0}), equalTo(local_zero.towsComplement()));
+            assertThat(BinaryNumber.of(new byte[]{1,1,1,1,0,0,1,1}), equalTo(noZeros.towsComplement()));
 	}
 
 	//divide
 
 	@Test
 	public void testDivideForPosNumbers(){
-		BinaryNumber thirty = BinaryNumber.of(new byte[]{1,1,1,1,0});
-		BinaryNumber twenty = BinaryNumber.of(new byte[]{1,0,1,0,0});
-		BinaryNumber five = BinaryNumber.of(new byte[]{1,0,1});
-		BinaryNumber six = thirty.divide(five);
-		BinaryNumber four = twenty.divide(five);
-		BinaryNumber one = thirty.divide(twenty);
-		BinaryNumber zero = twenty.divide(thirty);
-		assertEquals(BinaryNumber.of(new byte[]{0,1,1,0}), six);
-		assertEquals(BinaryNumber.of(new byte[]{0,1,0,0}), four);
-		assertEquals(BinaryNumber.of(new byte[]{1}), one);
-		assertEquals(BinaryNumber.of(new byte[]{0}), zero);
+            BinaryNumber five = thirty.divide(six);
+            BinaryNumber local_zero = zero.divide(six);
+            BinaryNumber four = thirty.divide(seven);
+            assertThat(BinaryNumber.of(5), equalTo(five));
+            assertThat(BinaryNumber.of(0), equalTo(local_zero));
+            assertThat(BinaryNumber.of(4), equalTo(four));
 	}
 
 	@Test
 	public void testDivideForPosAndNegNumbers(){
-		BinaryNumber negThirty = BinaryNumber.of(-30);
-		BinaryNumber thirty = BinaryNumber.of(30);
-		BinaryNumber five = BinaryNumber.of(new byte[]{1,0,1});
-		BinaryNumber negFive = BinaryNumber.of(-5);
-		BinaryNumber negSixDirectionOne = negThirty.divide(five);
-		BinaryNumber negSixDirectionTwo = thirty.divide(negFive);
-		BinaryNumber six = negThirty.divide(negFive);
-		assertEquals(BinaryNumber.of(-6), negSixDirectionOne);
-		assertEquals(BinaryNumber.of(-6), negSixDirectionTwo);
-		assertEquals(BinaryNumber.of(6), six);
+            BinaryNumber minusFive = minusThirty.divide(six);
+            BinaryNumber minusFive_inverted = thirty.divide(minusSix);
+            BinaryNumber five = minusThirty.divide(minusSix);
+            assertThat(BinaryNumber.of(-5), equalTo(minusFive));
+            assertThat(BinaryNumber.of(-5), equalTo(minusFive_inverted));
+            assertThat(BinaryNumber.of(5), equalTo(five));
 	}
 
 	@Test(expected = ArithmeticException.class)
-	public void testDivideForDivisionByZero(){
-		BinaryNumber zero = BinaryNumber.of(0);
-		BinaryNumber thirty = BinaryNumber.of(30);
+	public void testDivideForDivequalToionByZero(){
 		thirty.divide(zero);
-	}
-
-	@Test
-	public void testDivideForZeroAsNumerator(){
-		BinaryNumber zero = BinaryNumber.of(0);
-		BinaryNumber thirty = BinaryNumber.of(30);
-		BinaryNumber zeroAsResult = zero.divide(thirty);
-		assertEquals(BinaryNumber.of(0), zeroAsResult);
 	}
 }
